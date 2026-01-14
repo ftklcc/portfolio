b89a4ebd9cb47b6d8e255b3e4d525019fcc3db6b
Fuat Kılıç <ftklc@outlook.com>
2026-01-14 15:14:55 +0300

    fear: test commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+import { projects } from './projects.js'
+
+/* TAB BUTTONS */
+const tabButtons = document.querySelectorAll('.tab_button')
+const mainContents = document.querySelectorAll('.main__content')
+const projectCardList = document.querySelector('.projets__card-wrapper')
+
+
+const runEvents = () => {
+    showContent()
+    document.addEventListener('DOMContentLoaded', showCard)
+}
+/* show content  */
+const showContent = () => {
+    tabButtons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const id = e.target.dataset.id
+            if (!id) return;
+            tabButtons.forEach(button => button.classList.remove('active'))
+            mainContents.forEach(content => content.classList.remove('show'))
+
+            const el = document.getElementById(id)
+            e.target.classList.add('active')
+            el.classList.add('show')
+        })
+    })
+}
+/* show on uı projects */
+const showCard = () => {
+    projects.map(item => {
+        const li = document.createElement('li')
+        li.classList.add('project__card')
+        li.innerHTML = `
+							<div class="project__img-wrapper">
+								<img src="${item.image}" alt="project name" />
+							</div>
+							<div class="card__info">
+								<h3>${item.name}</h3>
+								<div class="card__actions">
+									${item.actions.map(el => `<a href="${el.link}" target="_blank">${el.name}</a>`)}
+								</div>
+								<div class="card__technology">
+									${item.technology.map(el => `<span class="${el}">${el}</span>`).join("")}
+								</div>
+							</div>`
+        projectCardList.appendChild(li)
+    })
+
+}
+// console.log(projects);
+
+
+
+
+
+runEvents()
---
+++ assets/js/projects.js
@@ -0,0 +1,74 @@
+export const projects = [
+    {
+        id: 1,
+        name: 'BMN Ticaret',
+        image: './assets/img/projects/bmn.jpg',
+        actions: [
+            {
+                name: 'Demo live',
+                link: 'https://e-ticaretwebsite.netlify.app/'
+            }
+        ],
+        technology: ['html', 'css', 'js']
+    },
+    {
+        id: 2,
+        name: 'pokemon api',
+        image: './assets/img/projects/pokemon.jpg',
+        actions: [
+            {
+                name: 'github',
+                link: 'https://github.com/ftklcc/Javascript-Projects/tree/main/Details-PokemonApi'
+            }
+        ],
+        technology: ['html', 'css', 'js']
+    },
+    {
+        id: 3,
+        name: 'Random Password',
+        image: './assets/img/projects/password.jpg',
+        actions: [
+            {
+                name: 'github',
+                link: 'https://github.com/ftklcc/Javascript-Projects/tree/main/Random%20Password'
+            }
+        ],
+        technology: ['html', 'css', 'js']
+    },
+    {
+        id: 4,
+        name: 'Detail Quiz App',
+        image: './assets/img/projects/quiz.jpg',
+        actions: [
+            {
+                name: 'github',
+                link: 'https://github.com/ftklcc/Javascript-Projects/tree/main/Detail-Quiz-App'
+            }
+        ],
+        technology: ['html', 'css', 'js']
+    },
+    {
+        id: 5,
+        name: 'CountDown Timer',
+        image: './assets/img/projects/count.jpg',
+        actions: [
+            {
+                name: 'github',
+                link: 'https://github.com/ftklcc/Javascript-Projects/tree/main/CountDown-Timer'
+            }
+        ],
+        technology: ['html', 'css', 'js']
+    },
+    {
+        id: 6,
+        name: 'Bootstrap Todo List',
+        image: './assets/img/projects/todo.jpg',
+        actions: [
+            {
+                name: 'github',
+                link: 'https://github.com/ftklcc/BootsTrap-Super-List'
+            }
+        ],
+        technology: ['html', 'css', 'js']
+    }
+]
